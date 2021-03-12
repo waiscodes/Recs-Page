c7709ff11874837360b9da9146c43b4682442e84
waiscodes <b.wais@outlook.com>
2021-03-12 16:33:24 -0700

    add recommendation:

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 const Dashboard = () => {
   return (
-    <div>
-      <p>hello</p>
-      <p>world</p>
-    </div>
+    <Card>
+      <Card.Body>
+        <div className='user-info'>
+          <img src='' alt='' />
+          <p className='display-name'>Birm Wais</p>
+          <p className='username'>waisideas</p>
+          <p>Add bio here</p>
+          <Button>Edit Profile</Button>
+          <Button>Add Recommendation</Button>
+        </div>
+      </Card.Body>
+    </Card>
   );
 };
 
