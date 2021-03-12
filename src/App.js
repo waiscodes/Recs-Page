25c3c5ef8dd85e4b2a2999080b3b00b2c9a5c1ac
waiscodes <b.wais@outlook.com>
2021-03-12 15:47:43 -0700

    added router through npm and added it to app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "./contexts/AuthContext";
-import ProfilePage from "./pages/ProfilePage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import SigninPage from "./pages/SigninPage";
 import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
-    <AuthProvider className='App'>
-      <Container>
-        <ProfilePage />
-        <SignupPage />
-      </Container>
-    </AuthProvider>
+    <Container>
+      <Router>
+        <AuthProvider className='App'>
+          <Switch>
+            <Route path='/signup' component={SignupPage} />
+            <Route path='/signin' component={SigninPage} />
+          </Switch>
+        </AuthProvider>
+      </Router>
+    </Container>
   );
 }
 
