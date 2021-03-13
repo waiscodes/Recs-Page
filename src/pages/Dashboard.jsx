0308000c30af0ad52de3632268b3712073a989ef
waiscodes <b.wais@outlook.com>
2021-03-12 17:16:48 -0700

    rendered user info from current user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Card, Button } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
+  const { currentUser } = useAuth();
+
   return (
     <Card>
       <Card.Body>
         <div className='user-info'>
           <img src='' alt='' />
           <p className='display-name'>Birm Wais</p>
-          <p className='username'>waisideas</p>
+          <p>{currentUser.email}</p>
           <p>Add bio here</p>
           <Button>Edit Profile</Button>
           <Button>Add Recommendation</Button>
