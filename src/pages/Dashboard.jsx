b2d3c8f3428242c68cf4d4398b02a9ce6a96c59c
waiscodes <b.wais@outlook.com>
2021-03-14 21:56:22 -0600

    imported and displayed default settings on dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,10 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { Card, Button } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
+import placeholderAvi from "../images/placeholder-avi.png";
 
 const Dashboard = () => {
+  const [avi, setAvi] = useState(placeholderAvi);
   const { currentUser } = useAuth();
   const testRef = useRef();
 
@@ -14,7 +16,7 @@ const Dashboard = () => {
     <Card>
       <Card.Body>
         <div className='user-info'>
-          <img src='' alt='' />
+          <img src={avi} alt='' />
           <p className='display-name'>Birm Wais</p>
           <p>{currentUser.email}</p>
           <p>Add bio here</p>
