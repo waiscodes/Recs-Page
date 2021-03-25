802b57b43f63f86787f189c1220c9941a4fd3a2f
waiscodes <b.wais@outlook.com>
2021-03-24 21:01:14 -0600

    added extra data to the profile

--- src/pages/CompleteProfile.jsx
+++ src/pages/CompleteProfile.jsx
@@ -6,7 +6,7 @@ import { Link, useHistory } from "react-router-dom";
 const CompleteProfile = () => {
   const emailRef = useRef();
   const nameRef = useRef();
-  const usernameRef = useRef();
+  const bioRef = useRef();
   const passwordRef = useRef();
   const conPasswordRef = useRef();
   const { signup } = useAuth();
@@ -20,22 +20,6 @@ const CompleteProfile = () => {
     if (passwordRef.current.value !== conPasswordRef.current.value) {
       return setError("Passwords do not match");
     }
-
-    try {
-      setError("");
-      setLoading(true);
-      await signup(
-        nameRef.current.value,
-        emailRef.current.value,
-        usernameRef.current.value,
-        passwordRef.current.value
-      );
-      setLoading(false);
-      history.push("/");
-    } catch {
-      setError("Failed to create account");
-      setLoading(false);
-    }
   };
 
   return (
@@ -46,17 +30,7 @@ const CompleteProfile = () => {
           {error && <Alert variant='danger'>{error}</Alert>}
           <Form onSubmit={handleSubmit}>
             <Form.Group>
-              <Form.Label className='d-none'>Email</Form.Label>
-              <Form.Control
-                type='email'
-                autoComplete='username'
-                placeholder='Email'
-                required
-                ref={emailRef}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label className='d-none'>Full Name</Form.Label>
+              <Form.Label className='d-none'>Display Name</Form.Label>
               <Form.Control
                 type='text'
                 placeholder='Full Name'
@@ -65,34 +39,17 @@ const CompleteProfile = () => {
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label className='d-none'>Username</Form.Label>
+              <Form.Label className='d-none'>Bio</Form.Label>
               <Form.Control
-                type='text'
-                placeholder='Username'
-                autoComplete='username'
-                required
-                ref={usernameRef}
+                as='textarea'
+                rows={3}
+                placeholder='Add your bio here'
+                ref={bioRef}
               />
             </Form.Group>
             <Form.Group>
-              <Form.Label className='d-none'>Password</Form.Label>
-              <Form.Control
-                type='password'
-                autoComplete='new-password'
-                placeholder='Password'
-                required
-                ref={passwordRef}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label className='d-none'>Confirm Password</Form.Label>
-              <Form.Control
-                type='password'
-                autoComplete='new-password'
-                placeholder='Confirm Password'
-                required
-                ref={conPasswordRef}
-              />
+              <Form.Label className=''>Profile Picture</Form.Label>
+              <Form.Control type='file' rows={3} ref={bioRef} />
             </Form.Group>
             <Button type='Submit' disabled={loading}>
               Sign up
