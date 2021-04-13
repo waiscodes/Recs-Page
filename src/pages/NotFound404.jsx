12ad5172f99525eae05fbd7613698957f1684f2a
waiscodes <b.wais@outlook.com>
2021-04-12 20:17:33 -0600

    added a user not found page

---
+++ src/pages/NotFound404.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const NotFound404 = () => {
+  return (
+    <>
+      <Container>
+        <h1>User Not Found</h1>
+      </Container>
+    </>
+  );
+};
+
+export default NotFound404;
