be4b554a6e008b334449b5c971858c48a770a910
waiscodes <b.wais@outlook.com>
2021-04-20 14:56:54 -0600

    added navbar sticky

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import "../css/MyNavbar.css";
 
 const MyNavbar = () => {
   return (
     <>
-      <Navbar bg='dark' variant='dark'>
+      <Navbar bg='dark' variant='dark' className='navbar' sticky='top'>
         <Navbar.Brand>Recs Page</Navbar.Brand>
         <Nav className='mr-auto'>
           <Nav.Link href='#home'>Home</Nav.Link>
