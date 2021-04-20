c55ac684148b74641ac74a4c5c4adb74806c5dd1
waiscodes <b.wais@outlook.com>
2021-04-20 14:48:22 -0600

    added a css document for the navbar

--- src/App.js
+++ src/App.js
@@ -10,27 +10,30 @@ import SignupPage from "./pages/SignupPage";
 import RecommendPage from "./pages/RecommendPage";
 import ProfilePage from "./pages/ProfilePage";
 import CompleteProfile from "./pages/CompleteProfile";
-import Navbar1 from "./pages/Navbar";
+import MyNavbar from "./pages/Navbar";
 
 function App() {
   return (
-    <Container>
-      <Router>
-        <AuthProvider className='App'>
-          <Switch>
-            <PrivateRoute exact path='/' component={Dashboard} />
-            <PrivateRoute
-              path='/complete-profile'
-              component={CompleteProfile}
-            />
-            <Route path='/signup' component={SignupPage} />
-            <Route path='/signin' component={SigninPage} />
-            <Route path='/recommend/:username' component={RecommendPage} />
-            <Route path='/:profile' component={ProfilePage} />
-          </Switch>
-        </AuthProvider>
-      </Router>
-    </Container>
+    <>
+      <MyNavbar />
+      <Container>
+        <Router>
+          <AuthProvider className='App'>
+            <Switch>
+              <PrivateRoute exact path='/' component={Dashboard} />
+              <PrivateRoute
+                path='/complete-profile'
+                component={CompleteProfile}
+              />
+              <Route path='/signup' component={SignupPage} />
+              <Route path='/signin' component={SigninPage} />
+              <Route path='/recommend/:username' component={RecommendPage} />
+              <Route path='/:profile' component={ProfilePage} />
+            </Switch>
+          </AuthProvider>
+        </Router>
+      </Container>
+    </>
   );
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
 
 const MyNavbar = () => {
   return (
     <>
-      <nav>hello world</nav>
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand>Recs Page</Navbar.Brand>
+        <Nav className='mr-auto'>
+          <Nav.Link href='#home'>Home</Nav.Link>
+          <Nav.Link href='#pricing'>Logout</Nav.Link>
+        </Nav>
+      </Navbar>
     </>
   );
 };
