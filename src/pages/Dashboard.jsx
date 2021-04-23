80a8a607c928ef8e981ade530f700acd0c34a116
waiscodes <b.wais@outlook.com>
2021-04-22 22:50:27 -0600

    added a link added to clipboard message

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -13,6 +13,7 @@ const Dashboard = () => {
   const [books, setBooks] = useState();
   const history = useHistory();
   const [bookSelected, setBookSelected] = useState(false);
+  const [copied, setCopied] = useState(false);
 
   const completeProfile = () => {
     history.push("/complete-profile");
@@ -62,6 +63,13 @@ const Dashboard = () => {
   const copyShareLink = () => {
     document.querySelector("#copyContent").select();
     document.execCommand("copy");
+    setCopied(true);
+
+    setTimeout(() => hideCopied(), 1000);
+
+    const hideCopied = () => {
+      setCopied(false);
+    };
   };
 
   const selected = {
@@ -92,20 +100,29 @@ const Dashboard = () => {
             className='screen-reader-text'
             defaultValue={user && "https://recs.page/" + user.username}
           />
-          <Button onClick={copyShareLink}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-clipboard'
-              viewBox='0 0 16 16'
-            >
-              <path d='M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z' />
-              <path d='M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z' />
-            </svg>{" "}
-            Profile Link
-          </Button>
+          <div className='text-center'>
+            <Button onClick={copyShareLink}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='16'
+                height='16'
+                fill='currentColor'
+                className='bi bi-clipboard'
+                viewBox='0 0 16 16'
+              >
+                <path d='M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z' />
+                <path d='M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z' />
+              </svg>{" "}
+              Profile Link
+            </Button>
+            <div className='copy-container'>
+              {copied && (
+                <p className='text-muted position-absolute '>
+                  Link copied to clipboard
+                </p>
+              )}
+            </div>
+          </div>
         </div>
         <hr />
         <RecommendPage uid={currentUser.uid} />
