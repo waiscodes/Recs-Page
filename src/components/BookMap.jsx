b2fedbf14250afa8b5dbb072d108c4c4a89c80f6
waiscodes <b.wais@outlook.com>
2021-05-14 11:36:56 -0600

    added an extra div around individual books to make grid easier.

--- src/components/BookMap.jsx
+++ src/components/BookMap.jsx
@@ -9,7 +9,9 @@ const BookMap = (props) => {
   return (
     <Container className='book-map'>
       {books?.map((book) => (
-        <Book key={book.id} bookObj={book} />
+        <div key={book.id} className='grid-item'>
+          <Book key={book.id} bookObj={book} className='grid-item' />
+        </div>
       ))}
     </Container>
   );
