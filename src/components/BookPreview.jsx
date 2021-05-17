f4372ec67585ec4189a929f5034554deb7819b5d
waiscodes <b.wais@outlook.com>
2021-05-16 21:17:34 -0600

    removed commented out code

--- src/components/BookPreview.jsx
+++ src/components/BookPreview.jsx
@@ -13,19 +13,6 @@ const BookPreview = ({ book, open }) => {
           className='thumbnail'
         />
       </div>
-      <div className='ind-book-desc'>
-        {/* <p>
-          {book?.title.length > 30
-            ? book?.title.substr(0, 25) + "..."
-            : book?.title}
-        </p>
-        <p className='recBy text-muted'>
-          Rec by{" "}
-          {book?.recBy.length > 10
-            ? book?.recBy.substr(0, 10) + "..."
-            : book?.recBy}
-        </p> */}
-      </div>
     </>
   );
 };
