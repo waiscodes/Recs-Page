f4c40f424071f4bff4e2dbeb5faabe3d461161c4
waiscodes <b.wais@outlook.com>
2021-05-17 17:39:50 -0600

    moved modal to its own component

--- src/components/BookMap.jsx
+++ src/components/BookMap.jsx
@@ -1,15 +1,11 @@
 import React, { useState } from "react";
-import { Container, Modal, Button } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Book from "./Book";
 import "../css/BookMap.css";
 
 const BookMap = (props) => {
   const books = props.books;
   const [bookSelected, setBookSelected] = useState();
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
 
   const closeDetails = () => {
     setBookSelected(null);
@@ -21,25 +17,6 @@ const BookMap = (props) => {
 
   return (
     <>
-      <div>
-        <Button variant='primary' onClick={handleShow}>
-          Launch demo modal
-        </Button>
-        <Modal show={show} onHide={handleClose} animation={false}>
-          <Modal.Header closeButton>
-            <Modal.Title>Modal heading</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={handleClose}>
-              Close
-            </Button>
-            <Button variant='primary' onClick={handleClose}>
-              Save Changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
-      </div>
       <Container className='book-map'>
         {books?.map((book) => (
           <Book
---
+++ src/components/DisplayModal.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Modal } from "react-bootstrap";
+
+const DisplayModal = () => {
+  return (
+    <>
+      <Button variant='primary' onClick={handleShow}>
+        Launch demo modal
+      </Button>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant='secondary' onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant='primary' onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default DisplayModal;
