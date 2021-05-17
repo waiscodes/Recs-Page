31bdef66203e08932ea3aa4c69033de49d82db73
waiscodes <b.wais@outlook.com>
2021-05-17 17:23:38 -0600

    added react-bootstrap modal from react-bootstrap

--- src/components/BookMap.jsx
+++ src/components/BookMap.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import { Container, Modal, Button } from "react-bootstrap";
 import Book from "./Book";
 import "../css/BookMap.css";
 
 const BookMap = (props) => {
   const books = props.books;
   const [bookSelected, setBookSelected] = useState();
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   const closeDetails = () => {
     setBookSelected(null);
@@ -16,18 +20,39 @@ const BookMap = (props) => {
   };
 
   return (
-    <Container className='book-map'>
-      {books?.map((book) => (
-        <Book
-          key={book.id}
-          book={book}
-          bookSelected={bookSelected}
-          selectBook={selectBook}
-          closeDetails={closeDetails}
-          className='book'
-        />
-      ))}
-    </Container>
+    <>
+      <div>
+        <Button variant='primary' onClick={handleShow}>
+          Launch demo modal
+        </Button>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant='secondary' onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant='primary' onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+      <Container className='book-map'>
+        {books?.map((book) => (
+          <Book
+            key={book.id}
+            book={book}
+            bookSelected={bookSelected}
+            selectBook={selectBook}
+            closeDetails={closeDetails}
+            className='book'
+          />
+        ))}
+      </Container>
+    </>
   );
 };
 
