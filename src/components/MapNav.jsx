c69525bc3ba45fd8ccfcf1abb6122cf67e5e55ba
waiscodes <b.wais@outlook.com>
2021-05-17 14:48:37 -0600

    Created a preview and details map navbar similar to twitter

---
+++ src/components/MapNav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MapNav = () => {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Previews</li>
+          <li>Details</li>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default MapNav;
