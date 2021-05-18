97ec43d206f32af69c688034f11ec727a70d2ea7
waiscodes <b.wais@outlook.com>
2021-05-17 18:17:04 -0600

    got book and displayed it in the modal

--- src/components/BookMap.jsx
+++ src/components/BookMap.jsx
@@ -7,20 +7,25 @@ import DisplayModal from "./DisplayModal";
 const BookMap = (props) => {
   const books = props.books;
   const [showModal, setShowModal] = useState(false);
+  const [displayBook, setDisplayBook] = useState(null);
 
   const handleClose = () => {
     setShowModal(null);
   };
 
   const handleShow = (bookId) => {
+    const book = books.filter((book) => {
+      return book.id === bookId;
+    });
+    setDisplayBook(book);
     setShowModal(true);
-    console.log(bookId);
   };
 
   return (
     <>
       <DisplayModal
         show={showModal}
+        displayBook={displayBook}
         handleShow={handleShow}
         handleClose={handleClose}
       />
--- src/components/DisplayModal.jsx
+++ src/components/DisplayModal.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Modal, Button } from "react-bootstrap";
 
-const DisplayModal = ({ show, handleShow, handleClose }) => {
+const DisplayModal = ({ show, displayBook, handleShow, handleClose }) => {
   return (
     <>
       <Modal show={show} onHide={handleClose} animation={false}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+          Woohoo, you're reading this text in a modal!
+          {JSON.stringify(displayBook, null, 2)}
+        </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
