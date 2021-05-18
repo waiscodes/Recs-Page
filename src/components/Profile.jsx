2c7278224b8723cf0c138efb06617b8a7255e956
waiscodes <b.wais@outlook.com>
2021-05-17 21:18:36 -0600

    added all the grid items in place and rounded the avi

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,16 +5,21 @@ const Profile = ({ user, children }) => {
   return (
     <>
       <div className='user-profile'>
+        <div className='header-container'>
+          <img src={user?.avi} className='header' alt='' />
+        </div>
         <img
           className='avi'
           alt='50x50'
-          src={user && user.avi}
+          src={user?.avi}
           data-holder-rendered='true'
         />
-        {children}
-        <h4 className='display-name'>{user && user.name}</h4>
-        <p className='text-muted'>{user && "@" + user.username}</p>
-        <p>{user && user.bio}</p>
+        <div className='edit'>{children}</div>
+        <div className='username'>
+          <h4 className='display-name'>{user?.name}</h4>
+          <p className='text-muted'>@{user?.username}</p>
+        </div>
+        <p className='bio'>{user?.bio}</p>
       </div>
     </>
   );
