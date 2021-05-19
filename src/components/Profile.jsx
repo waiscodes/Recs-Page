912e9252fe0800234da0b00cb859624f2d744447
waiscodes <b.wais@outlook.com>
2021-05-18 23:32:37 -0600

    moved header out of the profile container for better control in styling

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,17 +4,19 @@ import "../css/Profile.css";
 const Profile = ({ user, children }) => {
   return (
     <>
+      <div className='header-container'>
+        <img src={user?.avi} className='header' alt='' />
+      </div>
       <div className='user-profile'>
-        <div className='header-container'>
-          <img src={user?.avi} className='header' alt='' />
+        <div className='avi-edit'>
+          <img
+            className='avi'
+            alt='50x50'
+            src={user?.avi}
+            data-holder-rendered='true'
+          />
+          <div className='edit'>{children}</div>
         </div>
-        <img
-          className='avi'
-          alt='50x50'
-          src={user?.avi}
-          data-holder-rendered='true'
-        />
-        <div className='edit'>{children}</div>
         <div className='username'>
           <h4 className='display-name'>{user?.name}</h4>
           <p className='text-muted'>@{user?.username}</p>
