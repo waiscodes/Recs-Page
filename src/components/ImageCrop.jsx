bc4d1b6052a7b6659f01ab69d47487d9d71ddae8
waiscodes <b.wais@outlook.com>
2021-05-18 23:35:02 -0600

    added a React Image crop component

---
+++ src/components/ImageCrop.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Cropper from "react-easy-crop";
+
+const ImageCrop = () => {
+  const [crop, setCrop] = useState({ x: 0, y: 0 });
+  const [zoom, setZoom] = useState(1);
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    console.log(croppedArea, croppedAreaPixels);
+  }, []);
+
+  return (
+    <>
+      <Cropper
+        image={yourImage}
+        crop={crop}
+        zoom={zoom}
+        aspect={4 / 3}
+        onCropChange={setCrop}
+        onCropComplete={onCropComplete}
+        onZoomChange={setZoom}
+      />
+    </>
+  );
+};
+
+export default ImageCrop;
