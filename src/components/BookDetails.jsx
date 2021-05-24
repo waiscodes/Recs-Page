2dac2021065fe9c5cad580bc7c6c507b4bf8249d
waiscodes <b.wais@outlook.com>
2021-05-24 16:13:06 -0600

    aded styling on book details. moved title top and center and styled desc

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -4,26 +4,26 @@ import { CloseButton } from "react-bootstrap";
 const BookDetails = ({ book, close }) => {
   return (
     <>
-      <div className='book-desc'>
-        <CloseButton onClick={close}>Close</CloseButton>
-        <h4>{book?.title}</h4>
-        <p>
-          <span className='desc'>Author: </span>
-          {book?.author}
-        </p>
-        <p>
-          <span className='desc'>Rec by: </span>
-          {book?.recBy}
-        </p>
-        <p>
-          <span className='desc'>Reason: </span>
-          {book?.reason}
-        </p>
-        <p>
-          <span className='desc'>Upvotes: </span>
-          {isNaN(book?.rating) ? 0 : book?.rating}{" "}
-          {/* <span className='double-tap'>Double Tap to Like</span> */}
-        </p>
+      <CloseButton onClick={close}>Close</CloseButton>
+      <h4 className='book-title'>{book?.title}</h4>
+      <div className='book-detail'>
+        <div className='thumbnail-div'>
+          <img src={book?.thumbnail} alt='' />
+        </div>
+        <div className='book-desc'>
+          <p>
+            <span className='text-muted'>by: </span>
+            {book?.author}
+          </p>
+          <p>
+            <span className='text-muted'>Rec from: </span>
+            {book?.recBy}
+          </p>
+          <p>
+            <span className='text-muted'>Reason: </span>
+            {book?.reason}
+          </p>
+        </div>
       </div>
     </>
   );
