964ed57a79c796b80a377a5014438076cfb91902
waiscodes <b.wais@outlook.com>
2021-05-28 13:18:02 -0600

    changed the link on the nav bar for home from / to /home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const MyNavbar = () => {
         </Navbar.Brand>
         {currentUser ? (
           <Nav className='ml-auto'>
-            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/home'>Home</Nav.Link>
             <Nav.Link href='/welcome' onClick={() => signOut()}>
               Logout
             </Nav.Link>
