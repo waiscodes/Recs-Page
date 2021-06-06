6080dc5ac10865aea76e079657e120836aa2d4a1
waiscodes <b.wais@outlook.com>
2021-06-05 19:43:40 -0600

    removed paths that have been added to modals

--- src/App.js
+++ src/App.js
@@ -5,14 +5,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Dashboard from "./components/pages/Dashboard";
 import PrivateRoute from "./components/pages/PrivateRoute";
-import SigninPage from "./components/accounts/SigninPage";
-import SignupPage from "./components/accounts/SignupPage";
 import ProfilePage from "./components/pages/ProfilePage";
 import MyNavbar from "./components/templates/Navbar";
-import EditProfile from "./components/accounts/EditProfile";
 import Welcome from "./components/pages/Welcome";
 import NotFound404 from "./components/pages/NotFound404";
-import DisplayModal from "./components/DisplayModal";
 
 const App = () => {
   return (
@@ -23,10 +19,7 @@ const App = () => {
           <Container className='container'>
             <Switch>
               <PrivateRoute exact path='/' component={Dashboard} />
-              <PrivateRoute path='/edit-profile' component={EditProfile} />
-              <Route path='/signup' component={SignupPage} />
               <Route path='/welcome' component={Welcome} />
-              <Route path='/signin' component={SigninPage} />
               <Route path='/404/:username' component={NotFound404} />
               <Route path='/:profileUrl' component={ProfilePage} />
             </Switch>
