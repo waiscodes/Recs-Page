a05db801870787954d4beb94f2d8a50cdd7ec96b
waiscodes <b.wais@outlook.com>
2021-06-14 23:39:40 -0600

    Added modal and buttons to landing page

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,20 +1,40 @@
 import React, { useState } from "react";
-import SignIn from "../accounts/SignIn";
+import Authentication from "../accounts/Authentication";
+import { useAuth } from "../../contexts/AuthContext";
 import SignUp from "../accounts/SignUp";
+import DisplayModal from "../DisplayModal";
 import "../../css/Welcome.css";
+import { Button } from "react-bootstrap";
 
 const Welcome = () => {
-  const [account, setAccount] = useState(true);
+  const { currentUser, signOut } = useAuth();
+  const [haveAccount, setHaveAccount] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
+  const signUp = () => {
+    setHaveAccount(false);
+    setShowModal(true);
+  };
+  const signIn = () => {
+    setHaveAccount(true);
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(null);
+  };
   return (
     <>
       <div className='accounts'>
-        {account ? (
-          <SignIn setAccount={setAccount} />
-        ) : (
-          <SignUp setAccount={setAccount} />
-        )}
+        <DisplayModal show={showModal} handleClose={handleClose}>
+          <Authentication
+            handleClose={handleClose}
+            doesUserExist={haveAccount}
+          />
+        </DisplayModal>
       </div>
+      <Button onClick={signIn}>Sign In</Button>
+      <Button onClick={signUp}>Sign Up</Button>
     </>
   );
 };
