611218e234660137266b4e7fa7b9c2d62030d472
waiscodes <b.wais@outlook.com>
2021-07-08 12:24:50 -0600

    created an addbook component to simplify the recommend component

---
+++ src/components/addBook/AddBook.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddBook = () => {
+  return (
+    <div>
+      <h5>hello world from add add book component</h5>
+    </div>
+  );
+};
+
+export default AddBook;
