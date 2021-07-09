85ebe4fc526698583c3b2c780c5c02c5791331bb
waiscodes <b.wais@outlook.com>
2021-07-09 16:07:54 -0600

    moved the debouce search method over to add books

--- src/components/addBook/AddBook.jsx
+++ src/components/addBook/AddBook.jsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 const AddBook = () => {
+  const [title, setTitle] = useState();
+
+  const debounceSearch = useCallback(
+    debounce((title) => {
+      axios
+        .get(`https://www.googleapis.com/books/v1/volumes?q=${title}`)
+        .then((res) => {
+          if (res.data.items) {
+            setLoading(false);
+            setResult(res.data.items.slice(0, 5));
+          }
+        });
+    }, 500),
+    []
+  );
   return (
     <div>
       <h5>hello world from add add book component</h5>
